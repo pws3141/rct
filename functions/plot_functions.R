8b76574efb66b8fa7579daa1c3b8739cb66c5ef3
Paul Smith <psmith@posteo.net>
2025-05-27 16:13:45 +0100

    added test bslib file

--- functions/plot_functions.R
+++ functions/plot_functions.R
@@ -1,5 +1,65 @@
 ## Plotting functions
 
+## Bar Plot
+
+# ---- Create Bar Plot ----
+create_bar_plot <- function(plot.data) {
+  ggplot(plot.data, aes(x = year, y = proportion, fill = outcome)) +
+  geom_col(width = 0.8)  +
+  geom_label_repel( # add percentage labels for each section of box plot
+    aes(label = scales::percent(proportion, accuracy = 1)),
+    position = position_stack(vjust = 0.5),
+    size = 6,
+    colour = "black",
+    direction = "x",
+    box.padding = 0.5,
+    segment.color = NA,
+    min.segment.length = Inf,
+    max.overlaps = Inf
+  ) +
+  scale_x_discrete(labels = function(x) paste("By the end of year", x)) +
+  scale_y_continuous(labels = scales::percent, limits = c(0, 1)) +
+  scale_fill_manual(values = colours) +
+  geom_text_repel(# add legend labels on last bar
+    data = dt_labels,
+    aes(x = year, y = ypos, label = outcome, color = outcome),
+    inherit.aes = FALSE,
+    hjust = 0,
+    direction = "y",
+    xlim = c(3.42, 5),
+    size = 7,
+    fontface = "bold",
+    min.segment.length = Inf,
+    max.overlaps = Inf
+  ) +
+  scale_color_manual(values = colours) +
+  coord_cartesian(clip = "off") +   
+  labs(caption = "Outcomes after listing for a kidney transplant",
+       #fill = "Outcome",
+       #x = "Time after listing", 
+       y = "Proportion of patients") +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 0, 
+                               margin = margin(t = -7, b = 10), 
+                               size = 14,
+                               face = "bold"),
+    legend.position = "none",
+    # remove x axis ticks
+    axis.ticks.x = element_blank(),
+    # remove x-axis and y-axis title
+    axis.title = element_blank(),
+    # remove y-axis labels
+    axis.text.y = element_blank(),
+    # change plot.caption to left alligned
+    plot.caption = element_text(hjust = 0, size = 14, face = "bold"),
+    # remove grid lines
+    panel.grid = element_blank(),
+    # increase right-hand margin
+    plot.margin = margin(t = 0, r = 90, b = 0, l = 0, unit = "pt")
+  )
+}
+
 ## Icon plot
 
 # ---- Create Label Plot ----
